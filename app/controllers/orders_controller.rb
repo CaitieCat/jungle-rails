dbcb8fbcd615f43a667cdf6707284a5381a95903
Caitie Garrood <caitlinanneg@gmail.com>
2021-04-06 18:50:56 -0400

    Adds about page with route and description

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,6 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-
   end
 
   def create
@@ -10,8 +9,8 @@ class OrdersController < ApplicationController
     order  = create_order(charge)
 
     if order.valid?
-      empty_cart!
       redirect_to order, notice: 'Your Order has been placed.'
+      empty_cart!
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
     end
