34f45e6eac08f0ae83baa6178027a747b2557942
Caitie Garrood <caitlinanneg@gmail.com>
2021-04-06 21:38:17 -0400

    Adds user authentication feature

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class Admin::DashboardController < ApplicationController
+  before_filter :authorize
   http_basic_authenticate_with name: "Jungle", password: "book"
   def show
     @product_num = Product.count
