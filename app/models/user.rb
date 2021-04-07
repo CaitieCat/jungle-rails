ec0d2ea40008bfc6c97aa801e5f9bf9407fa9cee
Caitie Garrood <caitlinanneg@gmail.com>
2021-04-07 14:52:23 -0400

    Sets up testing for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
     has_secure_password
-
+    validates :password, presence: true, confirmation: true, length: { minimum: 8 }
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: { case_sensitive: false }
+    validates :quantity, presence: true
+    validates :category, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    user = User.new(
+      name: "Alice Alice",
+      email: "alice@alice.com",
+      password: "abcefg"
+    )
+    # validation tests/examples here
+    it "is not valid without a name" do
+      user.name = nil
+      expect(user).not_to be_valid
+    end
+    it "is not valid without an email" do
+      user.email = nil
+      expect(user).not_to be_valid
+    end
+    it "is not valid without a password" do
+      user.password = nil
+      expect(user).not_to be_valid
+    end
+  end
 end
